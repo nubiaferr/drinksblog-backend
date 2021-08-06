d7ed47952cc1843f4ef40c2371e337c956ee9def
nubiaferr <n.ferr@hotmail.com>
2021-08-05 22:42:06 -0300

    UserController - login/signin

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/UserController.java
@@ -0,0 +1,37 @@
+package org.generation.blogPessoal.controller;
+
+import java.util.Optional;
+
+import org.generation.blogPessoal.model.User;
+import org.generation.blogPessoal.model.UserLogin;
+import org.generation.blogPessoal.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/login")
+	public ResponseEntity<UserLogin> Authentication (@RequestBody Optional<UserLogin> userLogin){
+		return userService.Login(userLogin)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@PostMapping("/signin")
+	public ResponseEntity<User> Post(@RequestBody User user){
+		return ResponseEntity.status(HttpStatus.CREATED).body(userService.SignIn(user));
+	}
+
+}
