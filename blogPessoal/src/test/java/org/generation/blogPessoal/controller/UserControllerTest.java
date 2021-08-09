8e5f01e15d1c4d401c5fb75096dccfd6421e3caa
nubiaferr <n.ferr@hotmail.com>
2021-08-09 20:06:42 -0300

    J Unit complete

--- blogPessoal/src/test/java/org/generation/blogPessoal/controller/UserControllerTest.java
+++ blogPessoal/src/test/java/org/generation/blogPessoal/controller/UserControllerTest.java
@@ -12,39 +12,85 @@ import org.generation.blogPessoal.repository.UserRepository;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 
 @SpringBootTest (webEnvironment = WebEnvironment.RANDOM_PORT) //port aleatorio
 @TestInstance (TestInstance.Lifecycle.PER_CLASS) //quero testar classes
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserControllerTest {
+	
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+	private User user;
+	private User userUpdate;
+	private User userAdmin;
 	
 	@Autowired
 	private UserRepository userRepository;
 	
 	@BeforeAll
-	void start() {
-			LocalDate date = LocalDate.parse("2000-07-22", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-			
-			User user = new User(0, "João da Silva", "joao@email.com", "12345678", date);
-			if(userRepository.findByUsername(user.getUsername()).isEmpty()) {
-				userRepository.save(user);
-			} //se o usuario nao existir, criar
-			
-	
-			user = new User(0, "Frederico da Silva", "frederico@email.com.br", "13465278", date);
-			if(userRepository.findByUsername(user.getUsername()).isEmpty()) {
-				userRepository.save(user);
-			} 
-					
-			user = new User(0, "Paulo Antunes", "paulo@email.com.br", "13465278", date);
-			if(userRepository.findByUsername(user.getUsername()).isEmpty()) {
-				userRepository.save(user);
+	public void start(){
+		LocalDate dataAdmin = LocalDate.parse("1990-07-22",
+		DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		
+		userAdmin = new User(0L, "Administrador","admin@email.com.br", "admin123", dataAdmin);
+		if(!userRepository.findByUsername(userAdmin.getUsername()).isPresent()){
+				HttpEntity<User> request = new HttpEntity<User>(userAdmin);
+				testRestTemplate.exchange("/users/signin", HttpMethod.POST, request, User.class);
 			}
-		} 
+		
+		LocalDate dataPost = LocalDate.parse("2000-07-22",
+		DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		
+		user = new User(0L, "Paulo Antunes", "paulo@email.com.br", "13465278", dataPost);
+		LocalDate dataPut = LocalDate.parse("2000-07-22", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		
+		userUpdate = new User(2L, "Paulo Antunes de Souza",
+		"paulo_souza@email.com.br", "souza123", dataPut);
+		}
+		
+		@Test
+		@Order(1)
+		@DisplayName("✔ Sign in users!")
+		public void deveRealizarPostUsuario() {
+		HttpEntity<User> request = new HttpEntity<User>(user);
+		ResponseEntity<User> resposta = testRestTemplate.exchange("/users/signin", HttpMethod.POST, request, User.class);
+		assertEquals(HttpStatus.CREATED, resposta.getStatusCode());
+		}
+		
+		@Test
+		@Order(2)
+		@DisplayName("👍 List all users!")
+		public void deveMostrarTodosUsuarios() {
+			ResponseEntity<String> resposta = testRestTemplate
+					.withBasicAuth("admin@email.com.br", "admin123")
+					.exchange("/users/all", HttpMethod.GET, null, String.class);
+			assertEquals(HttpStatus.OK, resposta.getStatusCode());
+		}
+		
+		@Test
+		@Order(3)
+		@DisplayName("😳 Update user!")
+		public void deveRealizarPutUsuario() {
+		HttpEntity<User> request = new HttpEntity<User>(userUpdate);
+		ResponseEntity<User> resp = testRestTemplate
+		.withBasicAuth("admin@email.com.br", "admin123")
+		.exchange("/users/update", HttpMethod.PUT, request, User.class);
+		assertEquals(HttpStatus.OK, resp.getStatusCode());
+		}
 		
 		
 		@Test
---
+++ blogPessoal/src/test/java/org/generation/blogPessoal/model/UserTest.java
@@ -0,0 +1,53 @@
+package org.generation.blogPessoal.model;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class UserTest {
+	
+	public User user;
+	public User userNull = new User();
+	
+	@Autowired
+	private ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+	Validator validator = factory.getValidator();
+	
+	@BeforeEach
+	public void start() {
+	LocalDate date = LocalDate.parse("2000-07-22",
+	DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+	user = new User(0L, "João da Silva",
+	"joao@email.com.br", "13465278", date);
+	}
+	
+	@Test
+	@DisplayName("✔ Valida Atributos Não Nulos")
+	void testValidaAtributos() {
+	Set<ConstraintViolation<User>> violation = validator.validate(user);
+	System.out.println(violation.toString());
+	assertTrue(violation.isEmpty());
+	}
+	
+	@Test
+	@DisplayName("✖ Não Valida Atributos Nulos")
+	void testNaoValidaAtributos() {
+	Set<ConstraintViolation<User>> violation = validator.validate(userNull);
+	System.out.println(violation.toString());
+	assertTrue(violation.isEmpty());
+	}
+	
+
+
+}
